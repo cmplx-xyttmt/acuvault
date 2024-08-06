e9ae72c79152da8dfaedb2e2b8f68150704faba7
Isaac Owomugisha <archimedesisaac@gmail.com>
2024-08-06 23:05:27 +0300

    add create new space button (after fighting with hydration errors)

---
+++ components/ui/create_space.tsx
@@ -0,0 +1,126 @@
+'use client';
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription, DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {useState} from "react";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+    space_name: z.string().min(2).max(50),
+    category: z.string().min(2).max(50),
+    tags: z.string().min(2).max(50)
+});
+
+export default function CreateSpace() {
+    const [isOpen, setIsOpen] = useState(false);
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            space_name: '',
+            category: '',
+            tags: ''
+        },
+    });
+
+    const handleSubmit = (values: z.infer<typeof formSchema>) => {
+
+        // TODO: add the new space to a list
+        console.log(values);
+
+        setIsOpen(false);
+    }
+
+    return (
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <DialogTrigger asChild>
+                <Button>Create New Space</Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Space Details</DialogTitle>
+                    <DialogDescription>
+                        Enter the details for the space you're creating.
+                    </DialogDescription>
+                </DialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="space_name"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Space Name
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            id="space_name"
+                                            placeholder="Algorithms" {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="category"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Category
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            id="category"
+                                            placeholder="Computer Science" {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="tags"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Tags (comma separated)
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            id="tags"
+                                            placeholder="C++,Algorithms"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                        <DialogFooter>
+                            <Button type="submit">
+                                Create Space
+                            </Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
+
+            </DialogContent>
+        </Dialog>
+    );
+}
