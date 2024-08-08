bcaeff18eb10b5cb62d73025ca81cc18c338e51b
Isaac Owomugisha <archimedesisaac@gmail.com>
2024-08-08 15:04:15 +0300

    add list cards for spaces and quizzes

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,27 @@
 
 import Navbar from "@/components/main-nav";
 import CreateSpace from "@/components/ui/create_space";
+import CardList from "@/components/ui/CardList";
 
 export default function Home() {
     return (
         <>
             <Navbar/>
-            <CreateSpace/>
+            <div className="m-8">
+                <CreateSpace/>
+                <div className="my-4 grid grid-cols-2 gap-2">
+                    <CardList
+                        title="Spaces"
+                        description="Your list of spaces"
+                        items={["Algorithms", "NextJS", "Systems Programming"]}
+                    />
+                    <CardList
+                        title="Quizzes"
+                        description="Your list of quizzes"
+                        items={["Number Theory Algorithms Quiz", "Server Side Rendering Quiz", "Memory Management Quiz"]}
+                    />
+                </div>
+            </div>
         </>
         // <main className="flex min-h-screen flex-col items-center justify-between p-24">
         //     <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
---
+++ components/ui/CardList.tsx
@@ -0,0 +1,26 @@
+import {Card} from "@/components/ui/card";
+import {CardContent, CardDescription, CardHeader, CardTitle} from "./card";
+
+type CardListProps = {
+    title: string,
+    description: string,
+    items: string[]
+}
+
+export default function CardList({title, description, items}: CardListProps) {
+
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>{title}</CardTitle>
+                <CardDescription>{description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                {items.map((item, index) => (
+                    <div key={index}>{item}</div>
+                ))
+                }
+            </CardContent>
+        </Card>
+    )
+}
