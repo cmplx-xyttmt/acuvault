bd5e75e166f5f1359e351b74c36547246bacac34
Isaac Owomugisha <archimedesisaac@gmail.com>
2024-08-23 22:06:15 +0300

    attempt to implement adding questions, not yet there

--- components/ui/create_quiz.tsx
+++ components/ui/create_quiz.tsx
@@ -18,25 +18,29 @@ import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 
-const formSchema = z.object({
+const createQuizFormSchema = z.object({
     quiz_name: z.string().min(2).max(50),
     spaces: z.string().min(2).max(50),
     tags: z.string().min(2).max(50)
 });
 
+const createQuestionFormSchema = z.object({
+    question: z.string().min(2).max(255),
+    answer: z.string().min(2).max(255),
+    tags: z.string().min(0).max(50)
+});
+
+type DialogContentProps = {
+    changeStage: () => void;
+}
+
 export default function CreateQuiz() {
     const [isOpen, setIsOpen] = useState(false);
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            quiz_name: '',
-            spaces: '',
-            tags: ''
-        }
-    });
-
-    const handleSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+    const [stage, setStage] = useState("creating-quiz");
+    console.log(stage);
+    const changeStage = () => {
+        if (stage === "creating-quiz")
+            setStage("adding-questions")
     }
 
     return (
@@ -44,91 +48,220 @@ export default function CreateQuiz() {
             <DialogTrigger asChild>
                 <Button>Create New Quiz</Button>
             </DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Quiz Details</DialogTitle>
-                    <DialogDescription>
-                        Enter the details for the quiz.
-                    </DialogDescription>
-                </DialogHeader>
-                <Form {...form}>
-                    <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
-                        <FormField
-                            control={form.control}
-                            name="quiz_name"
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormLabel>
-                                        Quiz Name
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            id="quiz_name"
-                                            placeholder="Algorithms Quiz" {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                        />
+            {
+                stage === "creating-quiz" ? <CreateQuizDialogContent changeStage={changeStage}/> :
+                    <AddQuestionsDialogContent/>
+            }
 
-                        <FormField
-                            control={form.control}
-                            name="spaces"
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormLabel>
-                                        Choose a space for this quiz
-                                    </FormLabel>
-                                    <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                        <FormControl>
-                                            <SelectTrigger>
-                                                <SelectValue placeholder="Select a space."/>
-                                            </SelectTrigger>
-                                        </FormControl>
-                                        <SelectContent>
-                                            <SelectItem value="0">Algorithms</SelectItem>
-                                            <SelectItem value="1">Frontend Development</SelectItem>
-                                            <SelectItem value="2">Systems Programming</SelectItem>
-                                        </SelectContent>
-                                    </Select>
-
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                        />
-
-                        <FormField
-                            control={form.control}
-                            name="tags"
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormLabel>
-                                        Tags (comma separated)
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            id="tags"
-                                            placeholder="stacks,queues,graphs"
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                        />
-                        <Button>
-                            Add questions
-                        </Button>
-                        <DialogFooter>
-                            <Button type="submit">
-                                Save Quiz
-                            </Button>
-                        </DialogFooter>
-                    </form>
-                </Form>
-
-            </DialogContent>
         </Dialog>
     )
+}
+
+function CreateQuizDialogContent({changeStage}: DialogContentProps) {
+    const form = useForm<z.infer<typeof createQuizFormSchema>>({
+        resolver: zodResolver(createQuizFormSchema),
+        defaultValues: {
+            quiz_name: '',
+            spaces: '',
+            tags: ''
+        }
+    });
+
+    const handleSubmit = (values: z.infer<typeof createQuizFormSchema>) => {
+        console.log(values);
+        changeStage();
+    }
+    return (
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Quiz Details</DialogTitle>
+                <DialogDescription>
+                    Enter the details for the quiz.
+                </DialogDescription>
+            </DialogHeader>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name="quiz_name"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>
+                                    Quiz Name
+                                </FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="quiz_name"
+                                        placeholder="Algorithms Quiz" {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="spaces"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>
+                                    Choose a space for this quiz
+                                </FormLabel>
+                                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Select a space."/>
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        <SelectItem value="Algorithms">Algorithms</SelectItem>
+                                        <SelectItem value="Frontend Development">Frontend Development</SelectItem>
+                                        <SelectItem value="2">Systems Programming</SelectItem>
+                                    </SelectContent>
+                                </Select>
+
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="tags"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>
+                                    Tags (comma separated)
+                                </FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="tags"
+                                        placeholder="stacks,queues,graphs"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+                    <DialogFooter>
+                        <Button type="submit">
+                            Save Quiz and Add Questions
+                        </Button>
+                    </DialogFooter>
+                </form>
+            </Form>
+
+        </DialogContent>
+    )
+}
+
+function AddQuestionsDialogContent() {
+    const form = useForm<z.infer<typeof createQuestionFormSchema>>({
+        resolver: zodResolver(createQuizFormSchema),
+        defaultValues: {
+            question: '',
+            answer: '',
+            tags: ''
+        }
+    });
+    const [submittedQuestions, setSubmittedQuestions] = useState<z.infer<typeof createQuestionFormSchema>[]>([])
+
+    const handleSubmit = (values: z.infer<typeof createQuestionFormSchema>) => {
+        setSubmittedQuestions([...submittedQuestions, values]);
+        console.log("Saving all questions")
+        console.log(`Question: ${values['question']}`);
+        console.log(`Answer: ${values['answer']}`);
+        console.log("All questions:");
+        console.log(submittedQuestions);
+    }
+
+    const handleSaveAndAdd = () => {
+        console.log("Saving current values and ")
+        console.log(form.getValues());
+        setSubmittedQuestions([...submittedQuestions, form.getValues()]);
+        form.reset();
+    }
+
+    return (
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Quiz Questions</DialogTitle>
+                <DialogDescription>
+                    Add a new question.
+                </DialogDescription>
+            </DialogHeader>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name="question"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>
+                                    Question
+                                </FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="question"
+                                        placeholder="What is an algorithm?" {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="answer"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>
+                                    Answer
+                                </FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="answer"
+                                        placeholder="An algorithm is a sequence of steps that lead to an outcome" {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="tags"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>
+                                    Tags (comma separated)
+                                </FormLabel>
+                                <FormControl>
+                                    <Input
+                                        id="tags"
+                                        placeholder="stacks,queues,graphs"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+                    <DialogFooter>
+                        <Button type="button" onClick={handleSaveAndAdd}>
+                            Add Another Question
+                        </Button>
+                        <Button type="submit">
+                            Save and Close
+                        </Button>
+                    </DialogFooter>
+                </form>
+            </Form>
+
+        </DialogContent>
+    );
 }
